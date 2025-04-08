4c53c2debb738e0fd9df15e7adc34de45d02521b
Farhanz21 <a179820@siswa.ukm.edu.my>
2025-04-08 22:06:54 +0800

    changes made to nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
 
           <div className="dropdown mx-3">
             <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
-              Services 🛠️
+              Services 
             </button>
             <ul className="dropdown-menu">
               <li><a className="dropdown-item" href="#">Resume Review</a></li>
